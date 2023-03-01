7b0c277ee327fc24b1359e72f5b84ecb2841838a
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2023-03-01 14:53:10 +0200

    done

---
+++ main.py
@@ -0,0 +1,71 @@
+s = []
+
+
+def product(*args: tuple, repeat=1):
+    """
+    :param args: tuple from start to end
+    :param repeat: no of repeated for
+    :return: list of all possible motifs from start to end of args
+    """
+
+    pools = [tuple(pool) for pool in args] * repeat  # [[0...n-k+1], [0..n-k+1],....t_list]]
+    result = [[]]
+    for pool in pools:
+        result = [x + [y] for x in result for y in pool]
+    for prod in result:
+        yield prod
+
+
+def cal_score(motif: list[int], DNA: list[str], k_mers: int):
+    consensus = ''
+    score = 0
+    aline_mat = [DNA[j][i:i + k_mers] for j, i in zip(range(len(DNA)), motif)]
+
+    print(aline_mat, motif)
+
+    for i in range(len(aline_mat[0])):
+        counts = {
+            'A': 0,
+            'C': 0,
+            'G': 0,
+            'T': 0
+        }
+        for j in range(len(aline_mat)):
+            counts[aline_mat[j][i]] += 1
+        score += max(counts.values())
+        consensus += max(counts, key=counts.get)
+    return score, consensus
+
+
+def BruteForceMotifSearch(DNA: list[str], t_row: int, n_col: int, k_motif: int):
+    global best_consensus
+    best_score = 0
+    best_motif = None
+    prob_s = product(range(n_col - k_motif + 1), repeat=t_row)
+    for i in prob_s:
+        motif = i
+        score, consensus = cal_score(motif, DNA, k_motif)
+        if score > best_score:
+            best_score = score
+            best_motif = motif
+            best_consensus = consensus
+
+    return best_motif, best_score, best_consensus
+
+
+def main():
+    DNA = ['GTACAG', 'TGACCG', 'AAAACG', 'CTAGCG']
+    t = len(DNA)
+    n = len(DNA[0])
+    k = 2
+    best_motif, best_score, consensus = BruteForceMotifSearch(DNA, t, n, k)
+
+    print(f"{'#' * 40}")
+    print(f" Best motif : {best_motif} \n"
+          f" Consensus motif: {consensus} ")
+    print(f" With best score: {best_score}")
+    print(f"{'#' * 40}")
+
+
+if __name__ == '__main__':
+    main()
